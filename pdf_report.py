e3829f520c8676ce323d670ee9db058156ecc99b
AnujaNag2004 <147582278+AnujaNag2004@users.noreply.github.com>
2026-07-04 16:45:30 +0530

    Update pdf_report.py

--- pdf_report.py
+++ pdf_report.py
@@ -1,34 +1,50 @@
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer # type: ignore
-from reportlab.lib.styles import getSampleStyleSheet # type: ignore
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+import os
 
-doc = SimpleDocTemplate("reports/interview_report.pdf")
-styles = getSampleStyleSheet()
-story = []
+def generate_pdf_report(question, transcript, relevance_score):
 
-story.append(Paragraph("AI Interview Report", styles['Title']))
-story.append(Spacer(1, 20))
+    os.makedirs("reports", exist_ok=True)
 
-story.append(Paragraph("Question:", styles['Heading2']))
-story.append(Paragraph(
-    "What is Machine Learning?",
-    styles['BodyText']
-))
+    pdf_path = "reports/interview_report.pdf"
 
-story.append(Spacer(1, 10))
+    doc = SimpleDocTemplate(pdf_path)
+    styles = getSampleStyleSheet()
+    story = []
 
-story.append(Paragraph("Candidate Answer:", styles['Heading2']))
-story.append(Paragraph(
-    "Machine learning is a subset of artificial intelligence that enables computers to learn from data.",
-    styles['BodyText']
-))
+    story.append(
+        Paragraph("AI Interview Report", styles['Title'])
+    )
 
-story.append(Spacer(1, 10))
+    story.append(Spacer(1, 20))
 
-story.append(Paragraph("Relevance Score: 82.97%", styles['BodyText']))
-story.append(Paragraph("Speaking Speed: 126 WPM", styles['BodyText']))
-story.append(Paragraph("Filler Words: 2", styles['BodyText']))
-story.append(Paragraph("Final Interview Score: 85/100", styles['BodyText']))
+    story.append(
+        Paragraph("Interview Question", styles['Heading2'])
+    )
 
-doc.build(story)
+    story.append(
+        Paragraph(question, styles['BodyText'])
+    )
 
-print("PDF report generated successfully!")
+    story.append(Spacer(1, 10))
+
+    story.append(
+        Paragraph("Candidate Answer", styles['Heading2'])
+    )
+
+    story.append(
+        Paragraph(transcript, styles['BodyText'])
+    )
+
+    story.append(Spacer(1, 10))
+
+    story.append(
+        Paragraph(
+            f"Relevance Score: {relevance_score:.2f}%",
+            styles['BodyText']
+        )
+    )
+
+    doc.build(story)
+
+    return pdf_path
